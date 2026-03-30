a842001c0333e4b9930665283acc65f8ed3dcc49
Sufia Yusoof Simjee <sufiasimjee5@gmail.com>
2026-03-30 18:15:57 +0800

    new

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,49 +23,46 @@ import social from "../assets/personalprojects/socialmedia.png";
 
 const projectsData = [
   {
-    title: "Home Appliance Management System",
+    title: "Home Appliance Management System , Windows Application",
     description:
-      "For an academic project, I developed a simple CRUD application using WinForms (C#) and Microsoft SQL Server...",
+      "For an academic project, I developed a simple CRUD application using WinForms (C#) and Microsoft SQL Server. This experience taught me how to design database schemas for SQL databases and optimize them for read/ write operations through normalization and denormalization techniques. I also learned about data input validation, data types, and UI controls.",
     tech: [".Net (C#)", "MYSQL", "Winforms"],
     github: "https://github.com/SufiaSimjee/HomeApplianceManagementSystem",
     projectImage: home,
-    period: "Jan 2023 – May 2023",
+    // period: "Jan 2023 – May 2023",
   },
   {
-    title: "Camping Product Stor",
+    title: "Camping Site, Website",
     description:
-      "Dynamic website using CSS, JavaScript, and PHP with database integration and server-side rendering.",
+      "For another academic project, I was tasked with creating a dynamic website to promote camping productsusing vanilla CSS, JavaScript, and PHP. From this experience, I learned how to set up an HTTP server, connect back-end programs to a database system, and handle form submissions. I also learned about the concept of server-side rendering and how it can help reduce CPU load on the client side.",
     tech: ["HTML", "CSS", "Javascript", "PHP", "MYSQL"],
     github: "https://github.com/SufiaSimjee/Social-Media-Campaign-Website",
     projectImage: social,
-    period: "Jan 2024 - May 2024",
   },
   {
-    title: "Inventory Management System",
+    title: "Inventory Management System, Web-Based Information System",
     description:
-      "Database system with automation for membership and payments using forms and reporting tools.",
+      "I was tasked as a solo developer to create an information system for a small business as a school project. From this experience, I learned to design an IMS using Laravel framework which could handle high volumes of data through pagination technique. I also became familiar with technical concepts such as the MVC architecture, user access control, and data migration and seeding.",
     tech: ["Laravel", "MySQL", "Bootstrap"],
     github: "https://github.com/SufiaSimjee/ZawMinMobile",
     projectImage: ims,
-    period: "Jan 2025 – May 2025",
+   
   },
   {
-    title: "University Idea Management System",
+    title: "University Idea Management System, Web Application",
     description:
-      "Full-stack system with idea submission, voting, commenting, and role-based access.",
+      "I was assigned to a group project as a full-stack developer. From this experience, I learned how to design and implement REST APIs, set-up NoSQL databases, and create single-page applications that could be used simultaneously by multiple users. I also gained experience in deploying projects live on cloud platforms such as Vercel and Render",
     tech: ["React", "Node.js", "MongoDB"],
     github: "https://github.com/SufiaSimjee/University",
     projectVideo: university,
-    period: "Jan 2025 – May 2025",
   },
   {
-    title: "Trekking Application",
+    title: "Trekking Application, Android Application",
     description:
-      "Android apps using Kotlin with CRUD operations, API integration, and user input validation.",
+      "For a school project, I was tasked with developing two Android apps using the Android framework (Kotlin) and React Native to store & display information about trekking programs. I learned many important aspects of creating CRUD applications for mobile, such as designing intuitive UI, managing local and remote data storage, handling user-input validation, and integrating google APIs for dynamic data retrieval",
     tech: ["Kotlin"],
     github: "https://github.com/SufiaSimjee/Trekking-Application",
     projectImage: mhike,
-    period: "Jan 2025 – May 2025",
   },
 ];
 
@@ -107,7 +104,7 @@ const Projects = () => {
                 github,
                 projectImage,
                 projectVideo,
-                period,
+                // period,
               },
               idx
             ) => (
@@ -171,12 +168,8 @@ const Projects = () => {
                 )}
 
                 <CardContent>
-                  <Typography variant="h6" fontWeight="600">
+                  <Typography variant="h6" fontWeight="600"  sx={{mb: 2 }}>
                     {title}
-                  </Typography>
-
-                  <Typography variant="subtitle2" color="text.secondary" mb={1}>
-                    {period}
                   </Typography>
 
                   <Stack direction="row" spacing={1} flexWrap="wrap" mb={1}>
